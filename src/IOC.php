bf5222cb9dff0044aeac33bfe452eb1eb8db4b18
Mohamed Amine <MohamedAmine1C@gmail.com>
2018-02-28 18:27:41 +0100

    add New Features to IOC
    - bind() to bind an instance to the container
    - register() to register a new service locator

--- src/IOC.php
+++ src/IOC.php
@@ -2,11 +2,10 @@
 
 namespace IOC;
 
-use Exception;
-
 class IOC
 {
-    private $classes = [];
+    private $instances = [];
+    private $aliases   = [];
 
     public static function container()
     {
@@ -15,20 +14,39 @@ class IOC
 
     public function build(string $className, array $arguments = [])
     {
-        $builder = new InstanceBuilder($className, $arguments);
-        return $this->classes[$builder->classResolver->getClassShortName()] = $builder->build();
+        $builder = new InstanceBuilder($this->aliases, $className, $arguments);
+        $instanceName = $builder->classResolver->getClassShortName();
+        return $this->instances[$instanceName] = $builder->build();
+    }
+
+    public function bind(string $className, \Closure $action)
+    {
+        if (array_key_exists($className, $this->instances)) {
+            throw new \Exception("$className can't be registred (already exisits)", 15);
+        }
+        
+        $this->instances[$className] = $action();
+    }
+
+    public function register($key, $aliase)
+    {
+        if (array_key_exists($key, $this->aliases)) {
+            throw new \Exception("$key aliase already exists", 16);
+        }
+
+        $this->aliases[$key] = $aliase;
     }
 
     public function get(string $class)
     {
-        if (isset($this->classes[$class])) {
-            return $this->classes[$class];
+        if (isset($this->instances[$class])) {
+            return $this->instances[$class];
         }
-        throw new Exception("$class does not exist", 1);
+        throw new \Exception("$class does not exist", 1);
     }
 
     public function __get(string $className)
     {
-        return $this->classes[$className];
+        return $this->instances[$className];
     }
 }
