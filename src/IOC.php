f4e191a26af1e869c9bc08aacae7851052b1de34
Mohamed Amine <cheribet.amine@gmail.com>
2020-03-17 20:47:56 -0400

    improve comments

--- src/IOC.php
+++ src/IOC.php
@@ -4,28 +4,21 @@ namespace IOC;
 
 class IOC
 {
-    /**
- 	 * The IOC container
- 	 *
- 	 * @var Container
-	 */
     private static $container;
 
     /**
- 	 * This static method return an instance of IOC class (this class).
- 	 *
-     * @return Container
-	 */
+     * This static method return an instance of IOC class (this class).
+     *
+     */
     public static function createContainer(): Container
     {
         return static::$container = new Container();
     }
 
     /**
- 	 * Return an instance of the Container class
- 	 *
- 	 * @return Container
-	 */
+     * Return an instance of the Container class
+     *
+     */
     public static function container(): Container
     {
         return static::$container;
